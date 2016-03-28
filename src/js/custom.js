700c610d9a084c66876c0c9be82f0328a4b833e4
Den <undeadvoron@gmail.com>
2016-03-28 03:23:43 +0300

    it's alive

--- src/js/custom.js
+++ src/js/custom.js
@@ -7,7 +7,7 @@
  * Main Engine
  *
  **********************************/
-var language = 'en-us';
+
 window.onload = function(){
 	/*
 	 * Create chart tabs
@@ -131,29 +131,113 @@ function createListItem(inner, action){
  *
  **********************************/
 
+var holder={};
+
 function chartFrame (language, formatter, src) {
-	console.log(formatter);
 	var format = formatter.replace(/\//,"\\/");
-	frameSetter(document.getElementById("project"), "src/js/draw_project.js");
-	frameSetter(document.getElementById("resource"), "src/js/draw_resource.js");
-	function frameSetter (container, js){
-		var frame = document.createElement('iframe');
-		frame.className = "chartFrame";
-		frame.setAttribute("src", "./src/frame.html");
-		container.innerHTML = "";
-		frame.onload = function (){
-			var frameDoc = frame.contentDocument || frame.contentWindow.document;
-			var lang = frameDoc.createElement("script");
-			lang.innerHTML = 'var language = "'+language+'";\nvar formatter = "'+format+'"';
-			frameDoc.head.appendChild(lang);
-			var langFile = frameDoc.createElement("script");
-			langFile.setAttribute("src", "../"+src);
-			frameDoc.head.appendChild(langFile);
-			var script = frameDoc.createElement("script");
-			script.setAttribute("src", "../"+js);
-			frameDoc.head.appendChild(script);
-		};
-		container.appendChild(frame);
-		return frame;
+
+	var tabsContainer = document.getElementsByClassName("nav-tabs")[0];
+	var tabs = tabsContainer.getElementsByTagName("li");
+
+	tabs[0].removeEventListener("click",holder.project);
+	tabs[1].removeEventListener("click",holder.resource);
+
+	holder.project = function(){
+		eventSetter(document.getElementById("project"), "src/js/draw_project.js", language, format, src, tabs[0]);
+		tabs[0].removeEventListener("click", holder.project);
+	};
+	holder.resource = function() {
+		eventSetter(document.getElementById("resource"), "src/js/draw_resource.js", language, format, src, tabs[1]);
+		tabs[1].removeEventListener("click",holder.resource);
+	};
+
+	if (tabs[0].className=="active") holder.project();
+	else tabs[0].addEventListener("click",holder.project);
+
+	if (tabs[1].className=="active") holder.resource();
+	else tabs[1].addEventListener("click",holder.resource);
+
+	var jsonger = JSON.stringify(anychart.format.locales[language], undefined, 2);
+	var jsonHolder = document.getElementById("json");
+	jsonHolder.innerHTML = "";
+	jsonHolder.style.position = "relative";
+	var jsonText = document.createElement("div");
+	//jsonText.style.position = "absolute";
+	//jsonText.style.height = "100%";
+	jsonText.className = "innerJson";
+	jsonText.appendChild(wheel(anychart.format.locales[language]));
+	jsonHolder.appendChild(jsonText);
+
+	console.log(wheel(anychart.format.locales[language]));
+
+}
+
+function wheel (code) {
+	var domObj = document.createElement("div");
+	for (var i in code) {
+		switch (typeof code[i]){
+			case ("object"):
+				if (Array.isArray(code[i])){
+					domObj.appendChild(newString(i+": ["));
+					var strings = [];
+					code[i].forEach(function(item){
+						if (typeof item == "string") strings.push("'"+item+"'");
+						else strings.push(item)
+					});
+					domObj.appendChild(newBlock(strings));
+					domObj.appendChild(newString("],"));
+					break;
+				}
+				domObj.appendChild(newString(i+": {"));
+				domObj.appendChild(wheel (code[i]));
+				domObj.appendChild(newString("}"));
+				break;
+			case ("string"):
+				domObj.appendChild(newString(i+": '"+code[i] + "',"));
+				break;
+		}
 	}
+	domObj.style.overflow = "auto";
+	domObj.style.paddingLeft = "10px";
+	return domObj
+}
+
+function newString(text){
+	var string = document.createElement("a");
+	string.innerHTML = text.toString();
+	return string;
+}
+
+// work only for arrays
+function newBlock(array){
+	var block = document.createElement("div");
+	block.appendChild(newString(array.join(", ")));
+	block.className = "textHolder";
+	return block;
+}
+
+function frameSetter (container, js, language, formatter, src){
+	var frame = document.createElement('iframe');
+	frame.className = "chartFrame";
+	frame.setAttribute("src", "./src/frame.html");
+	container.innerHTML = "";
+	frame.onload = function (){
+		var frameDoc = frame.contentDocument || frame.contentWindow.document;
+		var lang = frameDoc.createElement("script");
+		lang.innerHTML = 'var language = "'+language+'";\nvar formatter = "'+formatter+'"';
+		frameDoc.head.appendChild(lang);
+		var langFile = frameDoc.createElement("script");
+		langFile.setAttribute("src", "../"+src);
+		frameDoc.head.appendChild(langFile);
+		var script = frameDoc.createElement("script");
+		script.setAttribute("src", "../"+js);
+		frameDoc.head.appendChild(script);
+	};
+	container.appendChild(frame);
+}
+
+function eventSetter(target, path, language, format, src, tab){
+	console.log(tab);
+	//if (tab.className=="active") tab.removeEventListener("click",eventSetter);
+	frameSetter(target, path, language, format, src);
 }
